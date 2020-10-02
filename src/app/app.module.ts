ea80881793e78979cd6c64c5c4296c1b98d81a4b
Sebin Sabu <ten.in.seb@gmail.com>
2020-10-02 13:41:09 +0530

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,43 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http"
-
-
-
-
 import { AppComponent } from './app.component';
-import { TestComponent } from './old-components/test/test.component';
-import { EmployeeService } from './old-components/employee.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './old-components/material/material';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { MatModule } from './python/mat/mat.module';
 import { CartItemComponent } from './ecommerce/shopping-cart/cart/cart-item/cart-item.component';
 import { ProductItemComponent } from './ecommerce/shopping-cart/product-list/product-item/product-item.component';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     routingComponents,
     CartItemComponent,
     ProductItemComponent
@@ -51,12 +23,9 @@ import { ProductItemComponent } from './ecommerce/shopping-cart/product-list/pro
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MaterialModule,
     HttpClientModule,
-    MatModule
     
   ],
-  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
